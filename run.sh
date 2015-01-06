4d114256b46aadeffddd62dbf6b7363149e04a8c
kub1x <j.podlaha@gmail.com>
2015-01-06 22:31:28 +0100

    fixed running script example

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-java -jar crowler.jar --scenario ../kub1x.sowl --phantom "C:/Program Files/phantomjs-1.9.8/phantomjs.exe" --rdfDir results
+PHANTOM_PATH="C:/Program Files/phantomjs-1.9.8/phantomjs.exe"
+java -jar crowler.jar --scenario kub1x.sowl --rdfDir results --phantom "${PHANTOM_PATH}"
